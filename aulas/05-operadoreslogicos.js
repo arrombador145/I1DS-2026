aa3228a3108e0cc0b03f1136ae04771c9a38b3cd
arrombador145 <jonathan.g.silva@edu.senai.br>
2026-03-11 16:58:25 -0300

    começo do 05 e 06

---
+++ aulas/05-operadoreslogicos.js
@@ -0,0 +1,24 @@
+// operadores lógicos
+
+let soupobre = true;
+console.log("sou pobre?", soupobre);
+console.log("negação de soupobre:", !soupobre);
+
+let manha = true;
+let sono = false;
+console.log("-------------------------------");
+console.log("manhã:", manha, "| estou com sono?", sono);
+
+console.log("operador AND (E)");
+
+// comparador E (AND e OU (OR)
+console.log("operador AND (E):", manha && sono);
+console.log("operador OR (OU):", manha || sono);
+
+console.log("-------------------------------");
+console.log("manhã:", manha, "| estou com sono?", sono);
+
+// comparador E (AND e OU (OR)
+console.log("operador AND (E):", manha && sono);
+console.log("operador OR (OU):", manha || sono);
+
