9a25004479cd7c3a86e9e35a9358069b7a8fee72
arrombador145 <jonathan.g.silva@edu.senai.br>
2026-03-17 17:07:51 -0300

    começo do atvdd-03

---
+++ atvdd/03-exercicios-funcoes.js
@@ -0,0 +1,16 @@
+/*****************************************************
+Lista de Exercícios
+*****************************************************/
+/*
+1. Crie uma função que receba como parâmetro o ano de nascimento de uma pessoa a retorne sua idade.
+2. Crie uma função que receba a quantidade de combustível de um carro e retorne quantos Kms ele pode viajar com esse combustível. Considere que o
+ veículo faz uma média de 12 Km/L.
+*/
+
+var gasocarro = "72"
+var anonascimento = "1970"
+console.log("o ano de nascimento dessa pessoa é:", anonascimento)
+
+let num1 = 1970
+let num2 = 2026
+function subtarcao
