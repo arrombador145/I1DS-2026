88a3a5bf2c887d06f298feaf606ccceaae47c031
arrombador145 <jonathan.g.silva@edu.senai.br>
2026-03-17 16:36:10 -0300

    salvando o exerciciosdevetor e continuações

--- atvdd/exerciciosDevetor.js
+++ atvdd/exerciciosDevetor.js
@@ -7,3 +7,26 @@
     5. Exclua o elemento uva.
     6. Crie uma cópia do array apenas com os elementos com índices 2, 3 e 4.
 */
+
+var fruts = ["banana", "maçã", "pera", "uva", "morango"];
+console.table(fruts);
+console.log("agora, a tabela tem", fruts);
+
+fruts.push("tangerina");
+console.table(fruts);
+console.log("agora, a tabela tem", fruts, "elementos");
+
+fruts.unshift("goiaba");
+console.table(fruts);
+console.log("agora, a tabela tem", fruts, "elementos");
+
+let procurado = "uva";
+let indice = fruts.indexOf(procurado);
+console.log("A ", procurado, "está no indice:", indice);
+
+console.log("a", procurado, "está no indice", indice);
+fruts.splice(indice, 1);
+console.table(fruts);
+
+let frutsnovas = fruts.slice(2,5);
+console.table(frutsnovas);
