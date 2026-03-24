6b28f57378b8418abfe9b1a32c837972b684362b
arrombador145 <jonathan.g.silva@edu.senai.br>
2026-03-24 16:47:04 -0300

    pode ser que acabe a bateria de novo, atv 09

--- atvdd/09-diagonalprincipal.js
+++ atvdd/09-diagonalprincipal.js
@@ -2,4 +2,36 @@
 | 3 5 8 |
 | 1 9 2 |
 | 7 1 4 |
+0 e 0
+1 e 1
+2 e 2
+
+*
+
+
+var miti = [
+ [3, 5, 8],
+ [1, 9, 2],
+ [7, 1, 4]
+
+]
+
+console.table(miti);
+for (let i = 0; i < miti.length; i++) {
+     
+}
+
+
+for (let i = 0; i < 3; i++) {
+    console.table(miti[i][i]);
+}
 */
+console.log("-----------------------------------------------------")
+
+ let = motroz [
+ [8, 1, 3]
+ [2, 9, 1]
+ [4, 6, 2] 
+ ]
+
+ console.table(motroz);
