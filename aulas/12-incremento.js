7797acf55499380c95282ac26742030165eabfeb
arrombador145 <jonathan.g.silva@edu.senai.br>
2026-03-24 10:11:40 -0300

    vfgfcfcbvc

---
+++ aulas/12-incremento.js
@@ -0,0 +1,26 @@
+// operadores de incremento e decremento | pré e pós incremento
+// incremento => somar algo de uma variável
+// decremento => subtrair algo de uma variável
+
+let idade = 16;
+console.log(idade);
+//idade = idade + 1; //somei 1 na idade
+
+idade++
+console.log(idade);
+console.log("---------------------------------");
+
+//idade = idade - 1; //subtrai 1 na idade
+idade + 1
+console.log(idade)
+idade++
+console.log(idade)
+idade++
+console.log(idade)
+console.log("---------------------------------");
+let novaidade = idade++
+console.log("idade:", idade, "| nova idade: ", novaidade) // idade++ soma na mesma variável
+
+novaidade = idade++ 
+console.log("idade:", idade, "| nova idade: ", novaidade)
+
