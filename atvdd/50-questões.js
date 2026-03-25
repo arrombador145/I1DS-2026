5dead38a85cf992da085ae4903a3c10c558c109f
arrombador145 <jonathan.g.silva@edu.senai.br>
2026-03-25 17:06:02 -0300

    fiz 10 questões

--- atvdd/50-questões.js
+++ atvdd/50-questões.js
@@ -36,9 +36,63 @@ console.log("============================================")
 //5º
 let altu = 50
 let bes = 10
-let tasemideia = altu * bes
+let comp = 10
+let tasemideia = altu * bes * comp
 console.log("a área do paralelepípedo é: ", tasemideia)
 
 console.log("============================================")
 
-//
+//6º
+let valorproduto = 100
+let valordesconto = 10
+let valorpronto = valorproduto - valordesconto
+
+console.log("esse produto com desconto vai ficar: ", valorpronto)
+
+console.log("============================================")
+
+//7º
+let celcius = 2
+let fare = (2 * 9 / 5) + 32
+console.log("convertendo, 2°C vai virar: ", fare, "Fahrenheit")
+
+console.log("============================================")
+
+//8º
+let altura2 = 1.75
+let peso = 60.0
+let imc = peso / (altura2 * altura2)
+
+console.log("o indice de massa corporal dessa pessoa é: ", imc)
+
+console.log("============================================")
+
+//9º
+let notadele = 5
+let notadela = 7
+let notadeleo = 8
+
+let notatotal = 5 + 7 + 8
+console.log("a soma de 5 + 7 + 8 é: ", notatotal)
+
+console.log("============================================")
+
+//10º
+let a = 2
+let b = 4
+let c = 6
+let somaa = a * a + b + c
+
+console.log( "o resultado dessa equação é: ", somaa)
+
+console.log("============================================")
+
+//11º
+let hora = 1.5
+let minuto = 1.5 * 60
+let segundo = minuto * 60
+
+console.log("em minutos vira: ", minuto)
+console.log("em segundos vira: ", segundo)
+
+console.log("============================================")
